beb4ef349f2e9dec17d35d9eca8f4afc467a7ee6
TITanZK <503838819@qq.com>
2022-01-03 23:06:50 +0800

    按需添加 element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import {Button} from 'element-ui'
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
