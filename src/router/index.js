c01bab746e700bd5145bf5233adebc41248fb827
TITanZK <503838819@qq.com>
2022-01-03 22:44:53 +0800

    添加各个页面及路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,46 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('@/pages/Index.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/pages/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/pages/Register.vue')
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: () => import('@/pages/Detail.vue')
+  },
+  {
+    path: '/edit',
+    name: 'Edit',
+    component: () => import('@/pages/Edit.vue')
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: () => import('@/pages/Create.vue')
+  },
+  {
+    path: '/user',
+    name: 'User',
+    component: () => import('@/pages/User.vue')
+  },
+  {
+    path: '/my',
+    name: 'My',
+    component: () => import('@/pages/My.vue')
+  }
 ]
 
 const router = new VueRouter({
