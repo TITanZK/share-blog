28afb0eabfe9809c3df3c4a691a8a960bf7c0b31
TITanZK <503838819@qq.com>
2022-01-06 10:24:58 +0800

    删除无用代码

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import http from "@/http"
 
 import {Button, Input, Switch, Form, FormItem, Message} from 'element-ui'
 
@@ -12,11 +11,9 @@ Vue.use(Switch)
 Vue.use(Form)
 Vue.use(FormItem)
 
-Vue.prototype.$message = Message;
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
-
-Vue.prototype.$http = http
 
 new Vue({
   router,
