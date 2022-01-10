795ab7460e9045a6ceb94236e68ccbe32d80f1dd
TITanZK <503838819@qq.com>
2022-01-10 15:31:16 +0800

    添加 vue.config 配置信息

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
+  chainWebpack: config => {
+    //修改或新增html-webpack-plugin的值，在index.html里面能读取htmlWebpackPlugin.options.title
+    config.plugin('html').tap(args => {
+      args[0].title = 'Lime share blog'
+      return args
+    })
+  }
 }
